eee3e700b621fe37e5a445b7bc918576afa96156
siljeberiksen <siljberi@smint.no>
2022-02-09 13:01:00 +0100

    valg lagt inn

--- a.c
+++ a.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
 #include "a.h"
 
 void menySystem(){
      printf("Welcome to the alarm clock!");
+    printf("It is currently\n"); //fikse tidspunkt + dato for nå
+    printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n");
+    int status=1;
+    while(status){
+        char choice = getchar();
+        switch(choice){
+            case 's':
+                printf("Schedule alarm at which date and time?\n");
+                break;
+                //funksjon for å legge inn alarm
+            case 'l':
+                 //list comands
+                 break;
+            case 'c':
+                 printf("Cancel which alarm?\n");
+                 //funksjon for å avslutte alarm
+                 break;
+            case 'x':
+                printf("Goodbye!");
+                status= 0;
+                break;
+            case '\n': break; //Ignore enter
+            default:
+                printf("Not an option.\n");
+                printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n");
+        }
+    }
 }
