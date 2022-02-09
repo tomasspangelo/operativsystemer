58f2ea175a505e1e78005768e9499697f8b81423
Tomas Haugland Spangelo <tomas@spangelo.com>
2022-02-09 17:59:36 +0100

    A new alarm can now be added

--- a.c
+++ a.c
@@ -1,17 +1,26 @@
 #include "a.h"
+#include "b.h"
 
 void menySystem(){
-    printf("Welcome to the alarm clock!");
-    printf("It is currently\n"); //fikse tidspunkt + dato for nå
+    char time_str[20];
+    printf("Welcome to the alarm clock! ");
+    printf("It is currently %s\n", parse_seconds(time_str, time(NULL))); //fikse tidspunkt + dato for nå
     printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n");
     int status=1;
+    struct alarm alarms[10];
     while(status){
         char choice = getchar();
-        switch(choice){
-            case 's':
-                printf("Schedule alarm at which date and time?\n");
+        switch(choice)
+        {
+            case 's': 
+                printf("Schedule alarm at which date and time?: ");
+                char time_string[20];
+                getchar();
+                scanf("%[^\n]%*c", time_string);
+                pid_t pid = 0; // bytt ut 0 med det som returneres fra fork (eventuelt kan logikken flyttes til add_alarm)
+                add_alarm(alarms, pid, time_string);
+                printf("Scheduling alarm in %ld seconds\n", alarms[pid].seconds - time(NULL));
                 break;
-                //funksjon for å legge inn alarm
             case 'l':
                  //list comands
                  break;
@@ -23,10 +32,14 @@ void menySystem(){
                 printf("Goodbye!");
                 status= 0;
                 break;
-            case '\n': break; //Ignore enter
+            case '\n':
+                break; //Ignore enter
             default:
                 printf("Not an option.\n");
-                printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n");
         }
     }
+}
+
+int main() {
+    menySystem();
 }
