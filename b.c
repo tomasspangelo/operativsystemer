2720b37d39d6a8d4d38825351501579c4bc3933e
Tomas Haugland Spangelo <tomas@spangelo.com>
2022-02-09 15:59:17 +0100

    Functionality for removing alarm

--- b.c
+++ b.c
@@ -6,15 +6,19 @@ time_t parse_time(char time_string[]){
     return mktime(&t);
 }
 
-void add(struct alarm *a, int pid, char time_string[]) {
-    struct alarm new_alarm = {.seconds = parse_time(time_string), .process_id = pid};
+void add_alarm(struct alarm *a, int pid, char time_string[]) {
+    struct alarm new_alarm = {.seconds = parse_time(time_string), .process_id = pid, .active = 1};
     a[pid] = new_alarm;
 
+}
+
+void remove_alarm(struct alarm *a, int pid) {
+    a[pid].active = 0;
 }
 
 int main() {
     struct alarm a[] = {};
     int pid = 100;
-    add(a, pid, "2022-01-31 15:15:23");
+    add_alarm(a, pid, "2022-01-31 15:15:23");
     printf("%lu %d", a[pid].seconds, a[pid].process_id);
 }
--- b.h
+++ b.h
@@ -2,12 +2,15 @@
 #include <time.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 struct alarm {
     time_t seconds;
     pid_t process_id;
+    bool active;
 
 };
 
 time_t parse_time(char time_string[]);
-void add(struct alarm *a, int pid, char time_string[]);
+void add_alarm(struct alarm *a, int pid, char time_string[]);
+void remove_alarm(struct alarm *a, int pid);
