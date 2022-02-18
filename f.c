8d329f2ef51f60ef9ae738a1fdd24fc367e6e608
Silje Eriksen <silje@MacBook-Air.local>
2022-02-18 18:40:24 +0100

    Songs placed in folder

--- f.c
+++ f.c
@@ -4,33 +4,33 @@ void playSong(int song){
     switch(song){
         case 1:
             #ifdef __APPLE__
-                system("afplay ./bensound-jazzyfrenchy.mp3 -t 5");
+                system("afplay music/bensound-jazzyfrenchy.mp3 -t 5");
              #elif __linux__
-                 system("mpg123 ./bensound-jazzyfrenchy.mp3 -t 5");
+                 system("mpg123 music/bensound-jazzyfrenchy.mp3 -t 5");
             #endif
             break;
         
         case 2:
             #ifdef __APPLE__
-                system("afplay ./bensound-acousticbreeze.mp3 -t 5");
+                system("afplay music/bensound-acousticbreeze.mp3 -t 5");
              #elif __linux__
-                 system("mpg123 ./bensound-acousticbreeze.mp3 -t 5");
+                 system("mpg123 music/bensound-acousticbreeze.mp3 -t 5");
             #endif
             break;
 
         case 3:
             #ifdef __APPLE__
-                system("afplay ./bensound-memories.mp3 -t 5");
+                system("afplay music/bensound-memories.mp3 -t 5");
              #elif __linux__
-                 system("mpg123 ./bensound-memories.mp3 -t 5");
+                 system("mpg123 music/bensound-memories.mp3 -t 5");
             #endif
             break;
 
         case 4:
             #ifdef __APPLE__
-                system("afplay ./bensound-littleidea.mp3 -t 5");
+                system("afplay music/bensound-littleidea.mp3 -t 5");
              #elif __linux__
-                 system("mpg123 ./bensound-littleidea.mp3 -t 5");
+                 system("mpg123 music/bensound-littleidea.mp3 -t 5");
             #endif
             break;
 
