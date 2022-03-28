5068d3e0fec5649e0576c6c56360111acfc20f4f
Silje Eriksen <silje@dhcp-10-24-20-15.wlan.ntnu.no>
2022-03-28 16:41:09 +0200

    split input

--- p3/flush.c
+++ p3/flush.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 
 #define MAX_PATH 200
+
+// The entered
+// text is split into command name and arguments. Arguments are separated from each other and from the command
+// name by space (ASCII 0x20) or tab (0x09) characters.
 
 void printFilepath( char *path ){
      getcwd(path, MAX_PATH);
@@ -14,4 +19,25 @@ void printFilepath( char *path ){
 int main(void) {
     char path[MAX_PATH];
     printFilepath(path);
+    char *str;
+    scanf("%[^\n]%*c", str);
+    printf("%s", str);
+
+    char *found;
+
+    
+    printf("Original string: '%s'\n",str);
+
+    printf("Does it work?");
+    //system(str);
+
+    
+    while( (found = strsep(&str," ")) != NULL ){
+        printf("Her kommer det");
+        printf("%s\n",found);
+    }
+   
+        
+
+    return(0);
 }
