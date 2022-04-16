3cf6b277deb06d6c5a801bb575b2d84fe14cd164
apadget <aap@sf-nett.no>
2022-04-16 16:11:08 +0200

    Ryddet headerfil

--- p3/flush.c
+++ p3/flush.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include "flush.h"
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
 
 #define MAX_BACKGROUND 10
 char path[MAX_PATH];
--- p3/flush.h
+++ p3/flush.h
@@ -3,6 +3,11 @@
 #include <unistd.h> //fork()
 #include <stdlib.h> //exit()
 #include <signal.h> //kill()
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #define MAX_PATH 200
 
@@ -13,4 +18,10 @@ struct Process {
     char argv[MAX_PATH][MAX_PATH];
     int size;
 };
+
+void printFilepath( char *path );
+void add_process(struct Process *p, pid_t pid, int *index_stack, int *top, char argv[MAX_PATH][MAX_PATH], int size);
+struct Process remove_process(struct Process *p, pid_t pid, int *index_stack, int *top);
+void create_process(struct Process *p, int *index_stack, int *top, char *command, int size, char argv[MAX_PATH][MAX_PATH]);
+
 #endif
